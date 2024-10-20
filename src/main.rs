97e5fa157ab24e0be8194b41a6b9d7c60574731b
Anish kumar dubey <anishkumardubey@Anishs-MacBook-Pro-3.local>
2024-10-20 16:12:16 +0530

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#[derive(Debug)] // use this to print whole struct
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+
+struct Rect {
+    width: u32,
+    height: u32,
+}
+
+impl Rect {
+    fn area(&self) -> u32 { // here self is the current struct
+        self.width * self.height
+    }
+
+    fn perimeter(&self, num: u32) -> u32 { //addition parameters can passed
+        (2 * (self.height + self.width)) + num
+    }
+
+    fn debug() -> u32 { // this is like a static function in java class
+        7
+    }
+}
+
 fn main() {
     let ans = is_even(34545);
     println!("{}", ans);
@@ -7,6 +35,23 @@ fn main() {
 
     let name = String::from("Anish Kumar");
     println!("{}", get_str_len(name));
+
+    let user1 = User {
+        active: true,
+        username: String::from("anishkumar3232"),
+        email: String::from("aads@gmail.com"),
+        sign_in_count: 1,
+    };
+    println!("{:?}", user1); //println macro doesn't know on its own how to print a struct
+
+    //Attaching function to the struct
+    let rect = Rect {
+        width: 30,
+        height: 50,
+    }; //this rect will automatically get access to the area function
+    println!("{}", rect.area());
+    println!("{}", rect.perimeter(5));
+    println!("{}", Rect::debug());
 }
 
 fn is_even(x: i64) -> bool {
@@ -44,6 +89,7 @@ fn fib(num: u32) -> u32 {
     return second;
 }
 
-fn get_str_len(str: String) -> usize{
+fn get_str_len(str: String) -> usize {
     str.chars().count()
 }
+
